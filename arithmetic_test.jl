605938b2556f4ba8cf1e12a02a1ffaa64435f4f4
John Lapeyre <john.lapeyre@gmail.com>
2015-01-16 11:52:48 +0100

    tests for nary mmul mplus 	modified:   arithmetic_test.jl

--- arithmetic_test.jl
+++ arithmetic_test.jl
@@ -1,3 +1,5 @@
+using Base.Test
+
 ## replacement arithmetic
 
 @test mmul(1,1) === 1
@@ -17,6 +19,14 @@
 @test mpow(2,3) === 8
 @test mpow(2.0,3) === 2.0^3
 @test mpow(2,-3) === 1//8
+# bug: add nary methods
+@test mplus(1,1,1) === 3
+@test mplus(1,1,1,1) === 4
+@test mmul(1,2,3,4) === 24
 
 @test @jm 8 * 1//8 === 1
 @test @jm 4 * 1//3 === 4//3
+
+# Mxpr are never constructed, Expr is evaluated directly
+@test @jm 1 + 2 + 3 + 4 === 10
+@test @jm 1 * 2 * 3 * 4 === 24
