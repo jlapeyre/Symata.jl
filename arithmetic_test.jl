c971701d37170a729cdb23b40707929170c8c23e
John Lapeyre <john.lapeyre@gmail.com>
2015-01-16 03:07:18 +0100

    new file:   arithmetic_test.jl

---
+++ arithmetic_test.jl
@@ -0,0 +1,22 @@
+## replacement arithmetic
+
+@test mmul(1,1) === 1
+@test mmul(2.0,2) === 4.0
+@test mmul(4,1//3) == 4//3
+@test mmul(3,1//3) === 1
+@test mmul(6,1//3) === 2
+@test mmul(1//3,6) === 2
+@test mmul(2.0,3.0) === 6.0
+@test mplus(1,1) === 2
+@test mplus(1,1.0) === 2.0
+@test mplus(1//3,1//3) == 2//3
+@test mplus(2//3,1//3) === 1
+@test mdiv(2,3) == 2//3
+@test mdiv(2,1//3) === 6
+@test mdiv(2.0,3) == 2/3
+@test mpow(2,3) === 8
+@test mpow(2.0,3) === 2.0^3
+@test mpow(2,-3) === 1//8
+
+@test @jm 8 * 1//8 === 1
+@test @jm 4 * 1//3 === 4//3
