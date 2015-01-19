f9dda3bd15b4bed3e764e88702482a8e7a2db003
John Lapeyre <john.lapeyre@gmail.com>
2015-01-19 20:30:15 +0100

    check. adding tests fixing bugs. not finding many.

--- test/orderless_test.jl
+++ test/orderless_test.jl
@@ -10,12 +10,17 @@ let s,a
     @test @sj( 1 - 1 ) == 0
     @test @sj( 1 + 1 ) == 2
     
-    # Fix handling scope! so we can make this local !
     a = :a
     @test @sj( a ) == a
     @test @sj( 0 + a ) == a
     @test @sj( a + 0 ) == a
     @test @sj( a - 0 ) == a
+    @test @sj( 1 * a ) == a
+    @test @sj( 0 * a ) == 0
+    @test @sj( a * 1 ) == a
+    @test @sj( a * 0 ) == 0
+    @test @sj( a * -0 ) == 0
+    @test @sj( -0 * a ) == 0
 
     s = @sj(1 + a + b + c)
     s[1] = :z
@@ -32,6 +37,12 @@ let s,a
     @test margs(s) == @sj([q1,b,c,z])  # terms are not sorted    
 
     @test margs(@sj(a + a)) == [2,a]
+    @test margs(@sj(a + a + a)) == [3,a]
+    @test margs(@sj(a + a^2 + a^3)) == [a,a^2,a^3]
+    @test margs(@sj(a + 2*a^2 + a^3)) == [a,2*a^2,a^3]
+    @test margs(@sj(a + a * a + a^3)) == [a,a^2,a^3]
+    @test @sj(a + a * a - a * a) == a
+    @test margs(@sj(a + z + 10*a^2 + 2 * z^2)) == [a,10*a^2,z,2*z^2]
     
 end
 
