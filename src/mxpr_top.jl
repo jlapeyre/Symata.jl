1b51687eee113ef73d5711c62f29ee3f3438a5ab
John Lapeyre <john.lapeyre@gmail.com>
2015-01-23 20:46:13 +0100

    new file:   src/mxpr_top.jl 	new file:   test/orderless_test.jl

---
+++ src/mxpr_top.jl
@@ -0,0 +1,73 @@
+## Things that can't be loaded twice.
+
+## Symbol correspondence/translation between Julia and SJulia
+
+const JTOMSYM  =
+ Dict(
+      :(=) => :Set,
+      :(:=) => :SetDelayed,
+      :+ => :Plus,
+      :- => :Minus,
+      :* => :Times,
+      :^ => :Power,
+      :(=>) => :Rule, # Mma uses ->  (hmmm)
+      :vcat => :List,
+      :ref => :Part,
+      :cell1d => :List,   # curly brackets, but deprecated by julia
+      :comparison => :Comparison,
+      )
+
+const MTOJSYM = Dict{Symbol,Symbol}()
+for (k,v) in JTOMSYM  MTOJSYM[v] = k end
+
+function jtomsym(x::Symbol)
+    if haskey(JTOMSYM,x)
+        return JTOMSYM[x]
+    end
+    return x
+end
+
+mtojsym(s::SJSym) = mtojsym(symname(s))
+function mtojsym(x::Symbol)
+    if haskey(MTOJSYM,x)
+        return MTOJSYM[x]
+    end
+    return x
+end
+
+const OPTYPE  = Dict{Symbol,Symbol}()
+
+for op in (:(=), :(:=), :(=>), :Rule , :Power )
+    OPTYPE[op] = :binary
+end
+
+for op in (:Plus, :Times)
+    OPTYPE[op] = :infix
+end
+
+getoptype(s::SJSym) = getoptype(symname(s))
+
+function getoptype(x::Symbol)
+#    println("Cheking $x")
+    if haskey(OPTYPE,x)
+        return OPTYPE[x]
+    end
+    return :prefix
+end
+
+const COMPARISONSYMBOLS = Dict{Symbol,Bool}()
+
+for op in ( :(==), :(<), :(>), :(>=), :(<=), :(===),
+           :(!=), :(!==), :(<:))
+    COMPARISONSYMBOLS[op] = true
+end
+
+function is_comparison_symbol(x::Symbol)
+    if haskey(COMPARISONSYMBOLS,x)
+        return true
+    end
+    return false
+end
+is_comparison_symbol(x::SJSym) = is_comparison_symbol(symname(x))
+is_comparison_symbol(x) = false
+
---
+++ test/orderless_test.jl
@@ -0,0 +1,2 @@
+# fixed bug for this one
+# ex = @ex(  zb + za + 7 + 2 * x^r  + 2 + a + c + x^3 + x^2)
