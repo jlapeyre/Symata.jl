2f23d9944e4be2308ff2bd566e96c682f764aeba
John Lapeyre <john.lapeyre@gmail.com>
2015-01-24 21:28:06 +0100

    add downvalue tests

--- test/downvalues_test.jl
+++ test/downvalues_test.jl
@@ -2,11 +2,11 @@
 @ex fib(1) := 1
 @ex fib(2) := 1
 @ex fib(n_) := fib(n-1) + fib(n-2)
-@test @ex(fib(10)) == 55
+@testex fib(10) == 55
 @ex Clear(fib)   # does not remove definition
-@test @ex(fib(11)) == @ex(fib(10)) + @ex(fib(9))
+@testex fib(11) == fib(10) + fib(9)
 @ex ClearAll(fib)  # removes definition
-@test @ex(Head(fib(11))) == getsym(:fib)
+@testex Head(fib(11)) == fib
 
 ## Condition on pattern
 
@@ -14,8 +14,11 @@
 @ex g(x_Integer) = "int"
 @ex g(x_Float64) = "float"
 
-@test @ex(g(1)) == "cat"
-@test @ex(g(2)) == "int"
-@test @ex(g(2.0)) == "float"
+@testex  g(1) == "cat"
+@testex  g(2) == "int"
+@testex  g(2.0) == "float"
 
-@ex ClearAll(g)
+@ex ClearAll(g,h,a,b)
+@ex h(x_^2) := x
+@testex  h((a+b)^2) == a + b
+@ex ClearAll(h)
