f9443cfa84ddce6a1106da13d31e040ce6c550df
John Lapeyre <john.lapeyre@gmail.com>
2015-01-25 21:54:12 +0100

    new file:   src/protected_symbols.jl

---
+++ src/protected_symbols.jl
@@ -0,0 +1,63 @@
+for v in ( "Set", "Pattern", "SetJ" )
+    @eval begin
+        set_attribute(symbol($v),:HoldFirst)
+        set_attribute(symbol($v),:Protected)        
+    end
+end
+
+for v in ("Clear", "ClearAll", "SetDelayed", "HoldPattern", "Hold", "DumpHold",
+          "DownValues")
+    @eval begin
+        set_attribute(symbol($v),:HoldAll)
+        set_attribute(symbol($v),:Protected)
+    end
+end
+
+for v in ("Attributes",)
+    @eval begin
+        set_attribute(symbol($v),:HoldAll)
+        set_attribute(symbol($v),:Protected)
+        set_attribute(symbol($v),:Listable)        
+    end
+end
+
+for v in ("RuleDelayed","PatternTest")
+    @eval begin
+        set_attribute(symbol($v),:HoldRest)
+        set_attribute(symbol($v),:Protected)
+        set_attribute(symbol($v),:SequenceHold)        
+    end
+end
+
+for v in ("Timing","Allocated")
+    @eval begin
+        set_attribute(symbol($v),:HoldAll)
+        set_attribute(symbol($v),:Protected)
+        set_attribute(symbol($v),:SequenceHold)        
+    end
+end
+
+for v in ("Pi","E")
+    @eval begin
+        set_attribute(symbol($v),:Protected)
+        set_attribute(symbol($v),:ReadProtected)
+        set_attribute(symbol($v),:Constant)
+    end
+end
+
+for v in ("CompoundExpression",)
+    @eval begin
+        set_attribute(symbol($v),:Protected)
+        set_attribute(symbol($v),:ReadProtected)
+        set_attribute(symbol($v),:HoldAll) 
+    end
+end
+
+for v in ("Apply","Dump", "Cos", "Length","Plus","Times", "Blank",
+          "JVar", "MatchQ", "EvenQ", "OddQ",
+          "Replace", "ReplaceAll","TraceOn","TraceOff","FullForm",
+          "BI", "BF", "BuiltIns")
+    @eval begin
+        set_attribute(symbol($v),:Protected)        
+    end
+end
