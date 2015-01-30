25989ee8af1ab6ef4e6618912b0a56d74c9a9d27
John Lapeyre <john.lapeyre@gmail.com>
2015-01-30 19:16:35 +0100

    check test

--- src/mxpr_type.jl
+++ src/mxpr_type.jl
@@ -3,7 +3,13 @@ type SJSym{T}  <: AbstractSJSym
     val::Any
     attr::Dict{Symbol,Bool}
     downvalues::Array{Any,1}
+    age::UInt64    
 end
+
+## Retrieve or create new symbol
+#sjsym(s::Symbol) = SJSym{s}(s,s,Dict{Symbol,Bool}(),Array(Any,0))
+#sjsym(s::Symbol) = SJSym{s}(s,Dict{Symbol,Bool}(),Array(Any,0),Dict{Symbol,UInt64}())
+sjsym(s::Symbol) = SJSym{s}(s,Dict{Symbol,Bool}(),Array(Any,0),0)
 
 symname{T}(s::SJSym{T}) = T
 symval(s::SJSym) = getsym(s).val  # This is the key: Look up the copy in the table
@@ -129,9 +135,6 @@ listdownvalues(s::SJSym) = mxpr(:List,s.downvalues...)
 
 const SYMTAB = Dict{Symbol,SJSym}()
 
-## Retrieve or create new symbol
-#sjsym(s::Symbol) = SJSym{s}(s,s,Dict{Symbol,Bool}(),Array(Any,0))
-sjsym(s::Symbol) = SJSym{s}(s,Dict{Symbol,Bool}(),Array(Any,0))
 function getsym(s::Symbol)
     if haskey(SYMTAB,s)
         return SYMTAB[s]
