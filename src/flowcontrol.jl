17e8960afe8ca033fc82a06ca867b7ab77b319b9
John Lapeyre <john.lapeyre@gmail.com>
2015-02-11 15:17:58 +0100

    add overlooked files. implement more of Do 	modified:   src/flowcontrol.jl 	new file:   src/namedparts.jl 	new file:   src/sjiterator.jl

--- src/flowcontrol.jl
+++ src/flowcontrol.jl
@@ -48,6 +48,8 @@ Do(expr,[i,imax]) evaluates expr imax times with i localized taking values from 
   imax in increments of 1.
 Do(expr,[i,imin,imax]) evaluates expr with i taking values from imin to imax with increment 1.
   imin and imax may be symbolic.
+Do(expr,[i,imin,imax,di]) evaluates expr with i taking values from imin to imax with increment di.
+  imin, imax, and di may be symbolic.
 "
 
 function apprules(mx::Mxpr{:Do})
@@ -60,6 +62,7 @@ function do_doloop(expr,iter::SJIter1)
     do_doloop_kern(expr,iter.imax)
 end
 
+# TODO: prbly don't need to use kernel
 function do_doloop_kern1(expr,imax)
     start = one(imax)
     for i in start:imax
@@ -85,6 +88,7 @@ function do_doloop{T<:Real,V<:Real}(expr,iter::SJIter3{T,V})
     end        
 end
 
+# fields of iter may be symbolic
 function do_doloop(expr,iter::SJIter3)
     isym = gensym(string(iter.i))
     ex = replsym(deepcopy(expr),iter.i,isym)
@@ -95,7 +99,6 @@ function do_doloop(expr,iter::SJIter3)
     end        
 end
 
-# TODO model these on code above for SJIter3, which works.
 function do_doloop{T<:Real, V<:Real, W<:Real}(expr, iter::SJIter4{T,V,W})
     println("in 4")
     isym = gensym(string(iter.i))
@@ -106,28 +109,13 @@ function do_doloop{T<:Real, V<:Real, W<:Real}(expr, iter::SJIter4{T,V,W})
     end        
 end
 
-# function do_doloop(expr,iter::SJIter3)
-#     isym = gensym(string(iter.i))
-#     ex = replsym(deepcopy(expr),iter.i,isym)
-#     setsymval(isym,iter.imin)    
-#     for i in 1:(iter.loopmax)
-#         doeval(ex)
-#         setsymval(isym,doeval(mxpr(:Plus,isym,1)))
-#     end        
-# end
-
+# fields of iter may be symbolic
 function do_doloop(expr,iter::SJIter4)
-    do_doloop_kern4(expr,iter.i,iter.imin,iter.imax, iter.di)
-end
-
-# Same here. don't need this kernel
-function do_doloop_kern4{T<:Real,V<:Real,W<:Real}(expr,i,imin::T,imax::V,di::W)
-    isym = gensym(string(i))
-    ex = replsym(deepcopy(expr),i,isym)
-    for i in imin:di:imax
-        setsymval(isym,i)
+    isym = gensym(string(iter.i))
+    ex = replsym(deepcopy(expr),iter.i,isym)
+    setsymval(isym,iter.imin)    
+    for i in 1:(iter.loopmax)
         doeval(ex)
-    end    
+        setsymval(isym,doeval(mxpr(:Plus,isym,iter.di)))
+    end        
 end
-
-do_doloop_kern4(expr,i,imin,imax,di) = error("Not implemented yet.")
---
+++ src/namedparts.jl
@@ -0,0 +1,23 @@
+## Julia-level access of parts (arguments) of Mxpr's by name.
+# 
+# First, recall that Mxpr expressions have the form h(a,b,c,d...)
+# Parts can be accessed from the Julia level by eg  h[1] --> a
+# They can also be accesed by eg margs(h(a,b,c,d))[1]
+# The arguments (parts) have varying semantic interpretation depending on the head 'h'.
+# We can get cheap, consistent, semantic access to the parts using multiple dispatch.
+# For example if p is an instance of Mxpr{:Power}, we can write base(p) to get the base.
+# This works for getting, but not for setting yet.
+
+# There may be a performance penalty, in some cases, for getting the
+# args field of the Mxpr each time named access is used, rather
+# getting the args once and then indexing directly into them.
+
+Base.base(p::Mxpr{:Power}) = p[1]
+expt(p::Mxpr{:Power}) = p[2]
+
+expr(p::Mxpr{:Part}) = p[1]
+inds(p::Mxpr{:Part}) = (a = margs(p); slice(a,2:length(a)))
+
+# These not used yet
+terms(s::Mxpr{:Plus}) = margs(s)
+factors(p::Mxpr{:Times}) = margs(p)
