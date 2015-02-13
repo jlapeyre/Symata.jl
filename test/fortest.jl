231a530e28b3815d052b21b994c51d1cd6afe1d4
John Lapeyre <john.lapeyre@gmail.com>
2015-02-13 13:50:04 +0100

    fix bitrot in fortest.jl

--- test/fortest.jl
+++ test/fortest.jl
@@ -1,6 +1,14 @@
-# much faster than SJulia For. Not so convenient
-function addtom(n)
-    # setpart no longer exists
-    for i in 1:1000  doeval( mxpr(:SetPart, :(m) , i,  i + n)) end
+# This is an example of writing a function for SJulia in Julia, rather than
+# SJulia. This is less general and convenient but runs faster.
+#
+# sjulia> x = Range(1000);  # x = [1,2,...]
+# julia> addtom(11,:x)     # x = [12,13,...]
+#
+# This is the same thing is SJulia:
+# For( i=1, i <= Length(x), i = i + 1, x[i] = x[i] +  11)
+#
+function addtom(n,m)
+    mx = symval(m) # m is a symbol, symval(m) is the SJulia expression
+    for i in 1:length(mx)  doeval( mxpr(:Set, mxpr(:Part, mx , i),  i + n)) end
 end
 
