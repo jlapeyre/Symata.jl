6479df809110beb42e5c643cb17d97aa525ce30f
John Lapeyre <john.lapeyre@gmail.com>
2015-02-16 18:44:47 +0100

    add syntax for Function and Slot. not using it yet

--- src/mxpr_top.jl
+++ src/mxpr_top.jl
@@ -12,6 +12,8 @@ const JTOMSYM  =
       :- => :Minus,
       :* => :Times,
       :^ => :Power,
+      :$ => :Function, #  $(1+~2) -->  Function(Plus(1,Slot(2))), but we don't do anything yet with this
+      :~ => :Slot,     # we need to translate lone ~ , ie Slot into Slot(1)
       :(=>) => :Rule, # Mma uses ->  (hmmm)
       :(->) => :RuleDelayed, # Mma uses :>. Julia parser does not allow this
       :vcat => :List,
