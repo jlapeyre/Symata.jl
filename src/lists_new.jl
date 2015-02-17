982d326fce4981077fe57bfdb7a5238bd82d6576
John Lapeyre <john.lapeyre@gmail.com>
2015-02-17 22:50:19 +0100

    We are using the new Table, but the old one is not in the state I thought it was. I will restore it. 	modified:   src/lists.jl 	renamed:    src/list_new.jl -> src/lists_new.jl

--- src/lists_new.jl
+++ src/lists_new.jl
@@ -1,4 +1,4 @@
-## This Table is not to be loaded, but is for experimenting.
+## Newer Table.
 # Instead of localizing the iterating var iter in expr, we
 # find and record the positions in expr where iter occurs
 # and set all occurence in a loop each time before evaluating.
@@ -10,16 +10,15 @@
 # faster. Still don't understand.
 
 # This is also rather limited
-function apprules(mx::Mxpr{:TableNew})
+set_attribute(:TableNew, :HoldAll)
+function apprules(mx::Mxpr{:Table})
     expr = mx[1]
     iter = mx[2]
-    isym = get_localized_symbol(iter[1])
     exprpos = expression_positions(expr,iter[1])
     imax = meval(iter[2])
-#    issym = createssym(isym,Int)  ## Trying out Typed variable
-    ex = replsym(deepcopy(expr),iter[1],isym) # takes no time, for simple expression
-    args = do_table_new(imax,isym,ex,exprpos) # creating a symbol is pretty slow
-    removesym(isym)
+    #    ex = replsym(deepcopy(expr),iter[1],isym) # takes no time, for simple expression
+    ex = deepcopy(expr)
+    args = do_table_new(imax,iter[1],ex,exprpos) # creating a symbol is pretty slow
     mx1 = mxpr(:List,args) # takes no time
 #    mergesyms(mx1,:nothing) # not correct, but stops the merging
     setcanon(mx1)
@@ -27,22 +26,27 @@ function apprules(mx::Mxpr{:TableNew})
     return mx1
 end
 
-function do_table_new{T<:Integer}(imax::T,isym,exin,exprpos)
+# Making this a kernel is not only useful, but faster.
+function set_part_spec(expr,spec,val)
+    p = expr
+    if length(spec) == 2
+        p[spec[end]] = val
+    else
+        for k in 2:length(spec)
+            p = expr[spec[k]]
+        end
+        #    println("Setting $val"
+        p[spec[end]] = val
+    end
+end
+
+function do_table_new{T<:Integer}(imax::T,isym,ex::Mxpr,exprpos)
     args = newargs(imax)
-    sisym = getssym(isym)
-    #    setsymval(sisym,1)  # the old way to do it.
-    ex = exin
     clearsyms(ex) # Clear the iterator variable
-#    ss = NSYM(Any[1:10])  # the arg nonsense here.
+#    println(exprpos)
     @inbounds for i in 1:imax
-#        ss1 = copy(ss)
         for j in 1:length(exprpos)  # exprpos is a list of positions at which the itvar occurs in ex
-            ispec = exprpos[j]
-            p = ex
-            for k in 2:length(ispec)
-                p = ex[ispec[k]]
-            end
-            ex[ispec[end]] = i
+            set_part_spec(ex,exprpos[j],i)
         end
         unsetfixed(ex)   # force re-evaluation
         args[i] = doeval(ex)
@@ -52,29 +56,24 @@ function do_table_new{T<:Integer}(imax::T,isym,exin,exprpos)
     return args
 end
 
-# Test code.
-# The speed penalty for val::Any instead of val::Int, is factor of 10^4.
-# Setting isym.val is by far the slowest step in Table(i,[i,10^6])
-# Much slower than Mma and SBCL
-# If we us val::Array{Any,1}, the speed penalty is better by a couple
-# orders of magnitude
-
-type NSYM
-#    val::Int
-    val::Array{Any,1}
+# For symbols, either the iterator, or not.
+function do_table_new{T<:Integer}(imax::T,isym,ex::SJSym,exprpos)
+    args = newargs(imax)
+    if isym == ex
+        @inbounds for i in 1:imax
+            args[i] = doeval(i)
+            setfixed(args[i])
+            setcanon(args[i])
+        end
+    else
+        @inbounds for i in 1:imax
+            args[i] = doeval(ex)
+            setfixed(args[i])
+            setcanon(args[i])
+        end
+    end
+    return args
 end
-
-Base.copy(x::NSYM) = NSYM(copy(x.val))
-
-# function testset()
-#     ss = NSYM(Any[0])
-#     sum0 = 0
-#     for i in 1:10^6
-#         ss.val[1] = i
-#         sum0 += ss.val[1]
-#     end
-#     sum0
-# end
 
 ## Return positions in ex at which subx is a subexpression
 # Returns an array of arrays representing part specifications
@@ -84,7 +83,7 @@ Base.copy(x::NSYM) = NSYM(copy(x.val))
 # just [0], in which case, it means the head matchs.
 
 function expression_positions(ex,subx)
-    posns = Array(Any,0)
+    posns = Array(Array{Int,1},0)
     lev = Array(Int,10)
     clev::Int = 1
     lev[clev] = 0
