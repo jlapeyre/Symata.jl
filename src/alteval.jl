42d42862fe20a45b6898ffef7e9b2eb06e2a9caf
John Lapeyre <john.lapeyre@gmail.com>
2015-02-19 14:55:06 +0100

    testing speed of meval 	modified:   src/alteval.jl

--- src/alteval.jl
+++ src/alteval.jl
@@ -1,18 +1,12 @@
-## Code for investigating why Table slowed down a factor of 5.
+#  Fast, stripped down expression evaluation code for investigating
+#  speed of doeval, meval.  In particular, Table takes unevaled args
+#  and calls doeval. So we call meval1 instead in Table (for testing)
 
 # This is stripped down evaluation
 
 @inline meval1(x::Complex) = x.im == 0 ? x.re : x
 @inline meval1(x) = x
 @inline meval1(s::SJSym) = symval(s)
-
-# This does not help
-function evalargs(args,nargs)
-    @inbounds for i in 1:length(args)
-        res = meval1(args[i])
-        nargs[i] = res
-    end
-end
 
 function meval1(mx::Mxpr)
     args = margs(mx)
@@ -25,5 +19,9 @@ function meval1(mx::Mxpr)
     end
 #    mx.args = nargs
     nmx = mxpr(mhead(mx),nargs)
+    nmx = flatcanon!(nmx)
+    nmx = ev_upvalues(nmx)    
+    nmx = ev_downvalues(nmx)
+    merge_args_if_emtpy_syms(nmx)
     apprules(nmx)
 end
