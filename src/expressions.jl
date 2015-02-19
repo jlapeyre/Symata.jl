3645cad341f248968c384636f2a5b724f7a25b31
John Lapeyre <john.lapeyre@gmail.com>
2015-02-19 13:21:25 +0100

    reorganize code 	modified:   src/SJulia.jl 	new file:   src/expression_utils.jl 	modified:   src/expressions.jl

--- src/expressions.jl
+++ src/expressions.jl
@@ -1,33 +1,5 @@
 ## Expand, Apply, Reverse
 
-# This is applied at toplevel after expression has been constructed.
-# It is applied now to binomial expansion. Needs to be applied more
-# generally.
-function apply_upvalues_to_args!(mx::Mxpr)
-    syms = listsyms(mx)
-    goodsyms = Array(Symbol,0)
-    for sym in syms
-        if has_upvalues(sym)
-            push!(goodsyms,sym)
-        end
-    end
-    args = margs(mx)
-    for sym in goodsyms
-        mx = deep_apply_upvalues!(mx,sym)
-    end
-    return mx
-end
-
-function deep_apply_upvalues!(mx::Mxpr,sym)
-    args = margs(mx)
-    for i in 1:length(mx)
-        args[i] = deep_apply_upvalues!(args[i],sym)
-    end
-    return applyupvalues(mx,sym)
-end
-deep_apply_upvalues!(x,sym) = x
-
-
 function _doexpand(x)
     ! is_Mxpr(x) && return x
     n = length(x)
