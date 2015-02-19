008987f5c0a294196718fcfc0e79bfe9a3afc9c7
John Lapeyre <john.lapeyre@gmail.com>
2015-02-19 17:27:41 +0100

    add some documentation that does not belong to a head 	modified:   src/SJulia.jl 	new file:   src/misc_doc.jl

---
+++ src/misc_doc.jl
@@ -0,0 +1,11 @@
+## Documentation not attached to an SJulia Head
+
+@sjdoc Regex "
+r\"expr\" creates a regular expression (PCRE). This is a Julia DataType.
+"
+
+@sjdoc macros "
+If SJulia encounters a macro call in input, it first Julia-evaluates all the
+arguments then Julia-evaluates the macro and inserts it into the SJulia expression
+tree. For instance, big numbers and regular expressions are constructed this way.
+"
