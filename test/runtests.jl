fa1c84f0021c6d8ae1ccc123a0bb9710a77cb185
John Lapeyre <lapeyre.math122a@gmail.com>
2016-10-01 19:53:30 +0200

    debug

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,6 +5,9 @@ import Base.Test: @test
 
 @ex TimeOff()   # don't print hundreds of diagnostic lines
 
+# For debugging
+@ex VersionInfo()
+
 function runtests()
     eval(parse("@ex Tests()"))
 end
