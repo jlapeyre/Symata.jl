558db2d8e0367c8e01dd48da208743da3f8f8bd5
John Lapeyre <lapeyre.math122a@gmail.com>
2016-10-10 21:42:18 +0200

    bump Symata version in Symata 	modified:   src/symataconstants.jl

--- src/symataconstants.jl
+++ src/symataconstants.jl
@@ -1,4 +1,4 @@
-const SYMATA_VERSION = v"0.0.15"
+const SYMATA_VERSION = v"0.0.16"
 
 const NullMxpr = mxprcf(:Null)
 const Null = :Null  # In Mma, Null is a Symbol. But, the Mma REPL prints nothing when encountering it (sometimes)
