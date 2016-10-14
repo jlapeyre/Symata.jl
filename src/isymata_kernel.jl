e2ee4300b9be480dc8aae007541dfc25ff135b7d
John Lapeyre <lapeyre.math122a@gmail.com>
2016-10-14 02:11:58 +0200

    add notebook kernel for IJulia that runs Symata on startup. 	new file:   src/isymata_kernel.jl

---
+++ src/isymata_kernel.jl
@@ -0,0 +1,41 @@
+import IJulia
+
+# workaround #60:
+if IJulia.Compat.is_apple()
+    ENV["PATH"] = JULIA_HOME*":"*ENV["PATH"]
+end
+
+IJulia.init(ARGS)
+
+# import things that we want visible in IJulia but not in REPL's using IJulia
+import IJulia: ans, In, Out, clear_history
+
+pushdisplay(IJulia.InlineDisplay())
+
+ccall(:jl_exit_on_sigint, Void, (Cint,), 0)
+
+# the size of truncated output to show should not depend on the terminal
+# where the kernel is launched, since the display is elsewhere
+ENV["LINES"] = get(ENV, "LINES", 30)
+ENV["COLUMNS"] = get(ENV, "COLUMNS", 80)
+
+println(IJulia.orig_STDOUT[], "Starting kernel event loops.")
+
+# Does not work
+IJulia.load_string("using Symata")
+
+using Symata
+
+IJulia.watch_stdio()
+
+# workaround JuliaLang/julia#4259
+delete!(task_local_storage(),:SOURCE_PATH)
+
+# workaround JuliaLang/julia#6765
+eval(Base, :(is_interactive = true))
+
+isymata()
+
+IJulia.waitloop()
+
+
