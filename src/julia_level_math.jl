5e7bd193c83e75769d0f23a93cfd97094c51e1c2
John Lapeyre <lapeyre.math122a@gmail.com>
2016-10-18 00:02:18 +0200

    tests failed on last commit. deleted methods restored so that tests pass.

--- src/julia_level_math.jl
+++ src/julia_level_math.jl
@@ -12,13 +12,14 @@
 *(a::SJSym,b::SJSym) = mxpr(:Times,a,b)
 +(a::SJSym,b::Number) = mxpr(:Plus,b,a)
 
-# Symata probably depends on these too, but there is no test that
-# catches this dependence.
-# *(a::SJSym,b::Number) = mxpr(:Times,b,a)
-# +(a::SJSym,b::SJSym) = mxpr(:Plus,a,b)
-# +(a::Number,b::SJSym) = mxpr(:Plus,a,b)
-# ^(base::SJSym,expt::Integer) = mxpr(:Power,base,expt)
-# ^(base::SJSym,expt) = mxpr(:Power,base,expt)
+# Symata depends on some of these too.
+# Probably depends on all of them. But there I am not sure
+# there is a test for each one.
+*(a::SJSym,b::Number) = mxpr(:Times,b,a)
++(a::SJSym,b::SJSym) = mxpr(:Plus,a,b)
++(a::Number,b::SJSym) = mxpr(:Plus,a,b)
+^(base::SJSym,expt::Integer) = mxpr(:Power,base,expt)
+^(base::SJSym,expt) = mxpr(:Power,base,expt)
 
 ######
 
