0f8bd6de50105bd11f18025504df51bab2167b37
John Lapeyre <lapeyre.math122a@gmail.com>
2016-10-26 18:14:13 +0200

    * add Characters * document some String things

--- src/strings.jl
+++ src/strings.jl
@@ -7,26 +7,42 @@ function apprules(mx::Mxpr{:StringInterpolation})
     symstring(margs(mx)...)
 end
 
+### StringLength
+
 @sjdoc StringLength """
     StringLength(s)
 
 returns the length of the string `s`.
 """
-
 apprules(mx::Mxpr{:StringLength}) = length(mx[1])
+
+### ToString
 
 @sjdoc ToString """
     ToString(expr)
 
 return a string of the printed form or `expr`.
 """
-
 apprules(mx::Mxpr{:ToString}) = symstring(mx[1])
+
+### StringJoin
 
 @sjdoc StringJoin """
     StringJoin(s1,s2,...)
+    StringJoin([s1,s2,...])
 
 return the concatenated strings.
 """
+@mkapprule StringJoin
+@doap StringJoin(s::List) = join(margs(s))
+@doap StringJoin(args::String...) = join(args)
 
-apprules(mx::Mxpr{:StringJoin}) = join(margs(mx))
+### Characters
+
+@sjdoc Characters """
+    Characters(s)
+
+return a `List` of the characters in `String` `s`.
+"""
+@mkapprule Characters
+@doap Characters(s::String) = mxpr(:List, [string(x) for x in s]...)
