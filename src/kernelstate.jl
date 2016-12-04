c4bf8fd6263cbb97b35827c64b04b58df065343f
John Lapeyre <lapeyre.math122a@gmail.com>
2016-12-05 00:48:18 +0100

    Fix Out(-n) off by one bug

--- src/kernelstate.jl
+++ src/kernelstate.jl
@@ -229,8 +229,8 @@ end
 get_output_by_line() = get_output_by_line(-1)
 
 function get_output_by_line(lineno::Integer)
-    if lineno < 0 lineno = length(Output) + lineno end
-    idx = length(Output) - (get_line_number() - lineno)
+    if lineno < 0 lineno = length(Output) + lineno + 1 end
+    idx = length(Output) - (get_line_number() - lineno) + 1
     if idx <= length(Output) && idx > 0
         res = get_saved_output_by_index(idx)
     else
