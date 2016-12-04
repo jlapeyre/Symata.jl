d5871f72f877d49ae842c66831baa36450ba3aaf
John Lapeyre <lapeyre.math122a@gmail.com>
2016-12-05 00:47:36 +0100

    change Mathics() to MmaSyntax()

--- src/alternate_syntax.jl
+++ src/alternate_syntax.jl
@@ -8,33 +8,33 @@ function _init_symatasyntax()
             @eval using SymataSyntax
             return true
         catch
-            error("Unable to load 'SymataSyntax'. If the module is not installed, try `Pkg.add(\"SymataSyntax\")`")
+            error("Unable to load 'SymataSyntax'. If the module is not installed, read https://github.com/jlapeyre/SymataSyntax.jl`")
             return false
         end
     end
     true
 end
 
-@sjdoc Mathics """
-    Mathics()
+@sjdoc MmaSyntax """
+    MmaSyntax()
 
 enters a Mathematica-syntax mode. Type `ctrl-d` to return to the usual Symata mode.
 
 The Mathematica-syntax mode is a modified version of the mathics REPL (terminal interface).
 Input expressions must be valid Mathematica expressions. Output is printed as Mathematica expressions.
 
-The only relation to Mathematica is the syntax. This mode does not interact with Mathematica in any way and is independent
-of Mathematica and Wolfram language and all other WRI products.
-
 This mode uses the mathics parser to read input and the mathics formatter to write output. But, all evaluation
 is done by Symata. Symata often calls the python package sympy, and builtin Julia functions.
 
 This mode requires the Julia package `SymataSyntax`, which requires the python package `mathics`.
+
+The only relation to Mathematica is the syntax. This mode does not interact with Mathematica in any way and is independent
+of Mathematica and Wolfram language and all other Wolfram company products.
 """
 
-@mkapprule Mathics :nargs => 0
+@mkapprule MmaSyntax :nargs => 0
 
-@doap function Mathics()
+@doap function MmaSyntax()
     (! _init_symatasyntax()) && return
     SymataSyntax.mathics_REPL()
 end
