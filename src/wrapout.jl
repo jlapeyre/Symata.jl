48fdac70ecf6695af93865009a708d964f51446e
John Lapeyre <lapeyre.math122a@gmail.com>
2018-07-16 10:07:00 +0200

    write @symwarn

--- src/wrapout.jl
+++ src/wrapout.jl
@@ -6,13 +6,24 @@ in output.jl.
 """
 wrapout(x) = x
 
+## TODO: disable symwarn, so we are forced to use @symwarn below
 for f in ( :print, :println, :string, :warn, :error, :throw)
    @eval function $(Symbol("sym" * string(f)))(args...)
         $(f)(map(wrapout,args)...)
     end
 end
 
+"""
+    @symwarn(arg)
 
+Performs macro call `@warn` on `wrapout(arg)` so that
+Julia displays objects of type `Mxpr` correctly.
+"""
+macro symwarn(arg)
+    ex = Meta.parse("@warn(dummy)")
+    ex.args[3] = wrapout(arg)
+    return ex
+end
 
 @doc """
    symprintln(expr)
